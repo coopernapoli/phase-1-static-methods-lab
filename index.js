47f6b0bf6bab8c772806714966dc563f1063220e
Sean <napolisc10@gmail.com>
2023-01-18 13:33:12 -0700

    commit tree 7a796639bd21c160c5339c81fc5fd6156186c584

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-\'\s]/g, "");
+  }
+
+  static titleize(string) {
+    const articles = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    return string
+      .split(" ")
+      .map((word, i) => {
+        if (i === 0) {
+          return word[0].toUpperCase() + word.slice(1);
+        } else if (articles.indexOf(word) === -1) {
+          return word[0].toUpperCase() + word.slice(1);
+        } else {
+          return word;
+        }
+      })
+      .join(" ");
+  }
 }
